2303416e9dfc46593aacfc705fb4befa7a2c25fd
Oluwaferanmi <alabaaderonketoyin@gmail.com>
2025-08-25 15:51:00 +0100

    updated

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,6 +2,6 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
 });
 export default instance;
